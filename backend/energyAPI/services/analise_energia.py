09d3811f9d3548ee273beb47a1889c9864276f71
gabrielTeixeira0021 <charlesgabriel102@gmail.com>
2025-11-17 19:57:55 -0300

    Add function to calculate solar panel efficiency
    - Introduced a new function `calcular_rendimento` to compute the average hourly efficiency of solar panels.
    - Integrated data loading from a CSV file containing solar radiation data.
    - Calculated incident power and efficiency percentage based on panel area and radiation values.

--- backend/energyAPI/services/analise_energia.py
+++ backend/energyAPI/services/analise_energia.py
@@ -2,4 +2,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+from pathlib import Path
 
+# Obter dados 
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+DATA_PATH = BASE_DIR / 'dados' / 'painel_solar.csv'
+
+def calcular_rendimento():
+    """Calcula o rendimento médio por hora dos paineis solares
+        
+       returns: 
+        dict: Dados processados prontos para Json
+    """
+    
+    # puxar os dados do arquivo csv
+    df = pd.read_csv(DATA_PATH)
+    
+    # Calcula rendimento
+    area_painel = 10 # m^2 
+    df['potencia_incidente_kw'] = (df['radiacao_wm2'] * area_painel) / 1000
+    df['porcentagem_rendimento'] = (df['potencia_kw'] / df['potencia_incidente_kw'])
